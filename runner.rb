e49d09179dbc41ca710fb1e512080f8276cf19af
Mitch Kroska <kroskam8457@hotmail.com>
2016-05-08 17:40:22 -0500

    Add story generator

--- runner.rb
+++ runner.rb
@@ -7,6 +7,7 @@ txt = open(filename)
 
 trigram = Trigram.new(txt)
 trigram.analyze_content
+puts trigram.generate_story(["in","the"], 100).to_s
 binding.pry
 # in console type this...
-trigram.give_trigram("upon a")
+trigram.give_trigram(['upon','a'])
--- trigrams.rb
+++ trigrams.rb
@@ -18,19 +18,28 @@ class Trigram
   def populate_trigrams_hash
     counter = 0
     while counter < (@words.length - 3)
-      key = "#{@words[counter]} #{@words[counter + 1]}"
+      key = "#{@words[counter].downcase} #{@words[counter + 1].downcase}"
       if @trigrams_hash[key] == nil
         @trigrams_hash[key] = []        
       end
-      @trigrams_hash[key] << @words[counter + 2 ]    
+      @trigrams_hash[key] << @words[counter + 2 ].downcase    
       counter += 1
     end
   end
 
   def give_trigram(bigram)
-    bigram_hash = @trigrams_hash[bigram]
+    bigram_hash = @trigrams_hash[bigram.join(" ")]
     word = bigram_hash[(0..bigram_hash.length-1).to_a.sample]
-    bigram + ' ' + word
+    word
+  end
+
+  def generate_story(initial_bigram, words_in_story)
+    start_words = initial_bigram
+    story = [start_words[0], start_words[1]]
+    words_in_story.times do |i|
+      story << self.give_trigram([story[-2], story[-1]])
+    end
+    return story.to_s
   end
 
 end
